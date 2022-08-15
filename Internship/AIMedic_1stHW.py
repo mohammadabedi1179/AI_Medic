6f8957dcafbfab163b027b253a59d6f544528057
mohammad mahdi abedi <mohammadabedi1179@gmail.com>
2022-08-15 11:15:24 +0430

    add regressor class to aimedic

--- Internship/AIMedic_1stHW.py
+++ Internship/AIMedic_1stHW.py
@@ -1,4 +1,16 @@
+from dataclasses import dataclass
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_log_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from bidi.algorithm import get_display
+from arabic_reshaper import reshape
+from sklearn.model_selection import KFold
 
 class Preprocessing():
 
@@ -77,7 +89,7 @@ class Preprocessing():
       new_labels = []
 
       for label in labels:
-        if type(label) == list:
+        if type(label) != str:
           label = label[0]
         if label not in labels_dict.keys():
           labels_dict[label] = key
@@ -97,3 +109,68 @@ class Preprocessing():
           new_labels.append(labels_dict['UNKNOWN'])
 
     return new_labels     
+
+class Regssor(Preprocessing):
+
+  def __init__(self, training_path, test_path, features):
+    
+    Preprocessing.__init__(self, training_path, test_path)
+    self.features = features
+    self.data_dicts = Preprocessing.extract_features(self, self.features)
+  
+  def extract_arrays(self):
+    data_dicts = self.data_dicts
+    train_features = []
+    test_features = []
+    
+    for feature in self.features:
+      train_features.append(Preprocessing.label_encoder(self, data_dicts[0][feature]))
+      test_features.append(Preprocessing.label_encoder(self, data_dicts[1][feature], train=False))
+    train_features = np.array(train_features, dtype=np.int32).transpose()
+    test_features = np.array(test_features, dtype=np.int32).transpose()
+    train_labels = np.array(data_dicts[0]['price'], dtype=int).reshape(-1)
+  
+    return train_features, train_labels, test_features
+
+  def plot_results(self, features_array, labels_array, font_dict):
+    
+    font = font_dict
+    features = self.features
+    number_of_features = len(features)
+    plt.figure(figsize=(80, 80))
+    
+    for i in range(number_of_features):
+        plt.subplot(int(np.ceil(np.sqrt(number_of_features))), int(np.ceil(np.sqrt(number_of_features))), i + 1)
+        plt.scatter(features_array[:, i], labels_array)
+        persian_labels = [f'قیمت اجناس بر حسب {features[i]}', 'قیمت به ریال', features[i]]
+        persian_labels = [ get_display(reshape(persian_label)) for persian_label in persian_labels]
+        plt.title(persian_labels[0], fontdict=font)
+        plt.ylabel(persian_labels[1], fontdict=font)
+        #plt.xlabel(persian_labels[2], fontdict=font)
+        plt.xticks([0, max(features_array[:, i])])
+    
+    plt.show()
+
+  def training(self, training_features, training_labels, test_features, model=LinearRegression(), num_of_cv_split=5, shuffle=True, output=True):
+    kf = KFold(n_splits=num_of_cv_split, shuffle=shuffle)
+    losses = []
+    predictions = []
+
+    for train_indices, valid_indices in kf.split(training_features):
+        train_features, valid_features = training_features[train_indices], training_features[valid_indices]
+        train_labels, valid_labels = training_labels[train_indices], training_labels[valid_indices]
+        clf = model
+        clf.fit(train_features, train_labels)
+        predictions.append(clf.predict(test_features).reshape(-1))
+        valid_predictions = clf.predict(valid_features).reshape(-1)
+        loss = mean_squared_log_error(valid_labels, valid_predictions)
+        losses.append(loss)
+        prediction = np.mean(predictions, axis=0)
+
+        if output:
+          id = np.arange(test_features.shape[0]).reshape(-1)
+          output = {'id' : id, 'price' : prediction}
+          output = pd.DataFrame.from_dict(output)
+          output.to_csv(r'output.csv', index=False, header=True)
+        
+        return prediction, losses
--- Internship/test.py
+++ Internship/test.py
@@ -1,58 +1,20 @@
-from AIMedic_1stHW import Preprocessing
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVC
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_log_error
+from AIMedic_1stHW import Regssor, Preprocessing
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from bidi.algorithm import get_display
-from arabic_reshaper import reshape
-from sklearn.model_selection import KFold
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+import numpy as np
 
+features = ['دسته بندی' ,'برند', 'وزن', 'ظرفیت', 'کارکرد', 'جنس بدنه', 'پردازنده', 'درگاه', 'تعداد پورت', 'سازگار با', 'دستگاه های سازگار', 'سازنده پردازنده', 'سیستم عامل', 'ابعاد', 'نوع پورت', 'منبع تغذیه', 'جنس', 'ولتاژ', 'مناسب برای', 'نوع حافظه', 'مقدار حافظه', 'نسبت تصویر', 'حداکثر وزن قابل تحمل', 'نوع کابل', 'مدل پردازنده', 'سری پردازنده']
+reg = Regssor('data/train.csv', 'data/test.csv', features)
+# extrcting features
+training_features, training_labels, test_features = reg.extract_arrays()
 
-prep = Preprocessing('/home/mohammadabedi/Desktop/data/train.csv', '/home/mohammadabedi/Desktop/data/test.csv')
-features = ['برند', 'دسته بندی', 'وزن', 'کارکرد', 'پورت', 'منبع تغذیه']
-data_dicts = prep.extract_features(features)
-train_features = []
-test_features = []
-for feature in features:
-    train_features.append(prep.label_encoder(data_dicts[0][feature]))
-    test_features.append(prep.label_encoder(data_dicts[1][feature], train=False))
-
-train_features = np.array(train_features, dtype=np.int32).transpose()
-test_features = np.array(test_features, dtype=np.int32).transpose()
-train_labels = np.array(data_dicts[0]['price'], dtype=int).reshape(-1)
-
-# plot the features
-font = {'family': 'B Nazanin',
+#plotting results
+font = {'family': 'Arial',
         'weight': 'normal',
-        'size': 16,
+        'size': 8,
         }
-number_of_features = len(features)
-plt.figure(figsize=(80, 10))
-for i in range(number_of_features):
-    plt.subplot(number_of_features, 1, i + 1)
-    plt.scatter(train_features[:, i], train_labels)
-    persian_labels = [f'قیمت اجناس بر حسب {features[i]}', 'قیمت به ریال', features[i]]
-    persian_labels = [ get_display(reshape(persian_label)) for persian_label in persian_labels]
-    plt.title(persian_labels[0], fontdict=font)
-    plt.ylabel(persian_labels[1], fontdict=font)
-    plt.xlabel(persian_labels[2], fontdict=font)
-plt.show()
-kf = KFold(shuffle=True)
-for train_indices, test_indices in kf.split(train_features):
-    training_features, valid_features = train_features[train_indices], train_features[test_indices]
-    training_labels, valid_labels = train_labels[train_indices], train_labels[test_indices]
-    clf = DecisionTreeRegressor(min_samples_split=5)
-    #clf = RandomForestRegressor()
-    clf.fit(training_features, training_labels)
-    predictions = clf.predict(test_features).reshape(-1)
-    id = np.arange(34262).reshape(-1)
-    output = {'id' : id, 'price' : predictions}
-    output = pd.DataFrame.from_dict(output)
-    output.to_csv(r'output.csv', index=False, header=True)
-    predictions = clf.predict(valid_features).reshape(-1)
-    loss = mean_squared_log_error(valid_labels, predictions)
-    print(loss) 
+#reg.plot_results(training_features, training_labels, font_dict=font)
+predictions, losses = reg.training(training_features, training_labels, test_features, model=KNeighborsRegressor(n_neighbors=4))
+print(np.mean(losses))
+
