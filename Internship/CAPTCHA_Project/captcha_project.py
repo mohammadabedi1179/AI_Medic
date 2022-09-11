850fcb3bea368ebb3e82d3ea07348aec7afa3e44
mohammad mahdi abedi <mohammadabedi1179@gmail.com>
2022-09-11 10:09:56 +0430

    delete notebook part

--- Internship/CAPTCHA_Project/captcha_project.py
+++ Internship/CAPTCHA_Project/captcha_project.py
@@ -248,58 +248,6 @@ class Results():
         plt.imshow(img, cmap='gray')                             #show image used for prediction
         plt.title(preds_str[i])                                  #show predicted phrase as image title
       plt.show(fig)
-
-net = NN()
-
-training, validation, test = net.load_data("/content/drive/MyDrive/AIMedic/Internship/exam_project/data", 32, 0.75, 0.15)
-labels_dict = net.labels_dict
-words_dict = dict((v, k) for k, v in labels_dict.items())
-
-model = net.load_model()
-history = model.fit(training,validation_data=validation, epochs=200, callbacks=[net.call_back])
-
-re = Results(history, model)
-re.plot_loss_accuracy()
-re.test(test)
-
-def download_history():
-  import pickle
-  from google.colab import files
-
-  with open('Final_history.pkl', 'wb') as f:
-    pickle.dump(history.history, f)
-
-  files.download('Final_history.pkl')
-
-download_history()
-
 class History:
   def __init__(self, history):
     self.history = history
-
-if __name__=="__main__":
-  net = NN()
-  training, validation, test = net.load_data("/content/drive/MyDrive/AIMedic/Internship/exam_project/data", 32, 0.75, 0.15)
-  labels_dict = net.labels_dict
-  words_dict = dict((v, k) for k, v in labels_dict.items())
-  model = tf.keras.models.load_model('/content/drive/MyDrive/AIMedic/Internship/saved model/new', compile=False)
-  with open('/content/Final_history.pkl', 'rb') as f:
-        new_history = pickle.load(f)
-        history = History(new_history)
-  #command = input("Please insert your caommand:" + "\n" + "insert number 1 to plot training loss and accuracy history" +  "\n" + "insert number 2 to plot some model predictions on test dataset"+ "\n" + "and insert number 3 to start a new training process" + "\n")
-  command = ''
-  while command != 'exit':
-    command = input("Please insert your command:" + "\n" + "insert number 1 to plot training loss and accuracy history" +  "\n" + "insert number 2 to plot some model predictions on test dataset"+ "\n" + "and insert number 3 to start a new training process" + "\n")    
-    if command == '1':
-      re = Results(history, model)
-      re.plot_loss_accuracy()
-    elif command == '2':
-      re = Results(history, model)
-      re.test(test)
-    elif command == '3':
-      model = net.load_model()
-      epochs = int(input('please enter number of epochs (at least 100 would be desired)'))
-      history = model.fit(training,validation_data=validation, epochs=200, callbacks=[net.call_back])
-    else:
-      print('please insert right command')
-
